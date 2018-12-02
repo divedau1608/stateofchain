935938a29ac383632e1e95a62920c6f8b53ffddf
divedau1608 <40553024+divedau1608@users.noreply.github.com>
2018-12-02 09:20:08 +0700

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  key: '5JFubEgK6zw5DXK2HWpqGFbtuRdvTtD9iEq3sY2G4VN17L8MwBS',
+  pKey: 'EVT5xAmSz6ZM8fsCAvPkT48tdBb8qqPzximo3U58epYWQuLSrqmXD',
+  prefixDR: 'DRT',
+  prefixMD: 'MDT',
+  prefixPT: 'PTT'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  key: '5JFubEgK6zw5DXK2HWpqGFbtuRdvTtD9iEq3sY2G4VN17L8MwBS',
+  pKey: 'EVT5xAmSz6ZM8fsCAvPkT48tdBb8qqPzximo3U58epYWQuLSrqmXD',
+  prefixDR: 'DRTT',
+  prefixMD: 'MDT',
+  prefixPT: 'PTT'
+};
